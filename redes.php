d408e81ef8730dfe267348d94424434442963033
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-07-18 10:10:54 -0600

    Mueve codigo de redes flotantes

---
+++ redes.php
@@ -0,0 +1,8 @@
+<div class="red">
+                        <div id="facebook"><a href="https://www.facebook.com/ABCImpulsaTi/" target="new" class="fa fa-facebook"></a></div>
+                        <div id="youtube"><a href="https://www.youtube.com/channel/UCfFY94dS15wL7U1zDLU3yMA" target="new" class="fa fa-youtube-play"></a></div>
+                        <div id="instagram"><a href="https://www.instagram.com/abcimpulsati/" target="new" class="fa-brands fa-instagram"></a></div>
+                        <!-- <div id="twitter"><a href="https://twitter.com/gruposlash" target="new" class="fa fa-twitter"></a></div> -->
+                        <div id="correo"><a href="https://www.linkedin.com/in/abc-impulsate-ti-bb856627b/"  target="new"class="fab fa-linkedin"> </a></div>
+                        <div id="tiktok"><a href="https://www.tiktok.com/@impulsati" target="new" class="fa-brands fa-tiktok"></a></i></div>
+                    </div>
