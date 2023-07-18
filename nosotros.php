7b4be3c3323566a9708a8a10b0cc1039ffff72f7
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-07-18 12:24:24 -0600

    Archivo listo

---
+++ nosotros.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php
+            require "main/head.php";
+        ?>
+    <title>Nosotros</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <?php
+            require "main/header.php";
+        ?>
+
+    </div>
+</body>
+<?php
+    require "main/footer.php";
+?>
+</html>
