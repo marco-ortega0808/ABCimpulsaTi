8175a0028eb0c36890e8aec696c3338be734651d
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-07-28 10:57:46 -0600

    Da formarto alerta

--- evento.php
+++ evento.php
@@ -44,26 +44,33 @@
                     </div>
                     <div class="col-2">
                         <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#exampleModal"> Ver más</button>
-                        <!-- Modal -->
+                        <!-- ...............Modal--------------------- -->
                         <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
                             <div class="modal-dialog">
                                 <div class="modal-content">
                                     <div class="modal-header">
-                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <h5 class="modal-title" id="exampleModalLabel">Más información del evento</h5>
                                         <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                     </div>
                                     <div class="modal-body">
-                                        <div class="text-center h2">zoom</div>
+                                        <div class="text-center h2">Zoom</div>
                                         <div class="text-center h3">Tema a tratar</div>
                                         <hr class="mt-4 mb-4">
                                         <div class="row">
                                             <div class="col-6">
-                                                <div class="h4"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" class="jsx-1364132950 eapp-events-calendar-time-icon"><path d="M6 12A6 6 0 1 0 6 0a6 6 0 0 0 0 12zm.984-4.97a.454.454 0 0 1-.07.154l-.026.038a.5.5 0 0 1-.655.148L3.55 5.857a.454.454 0 0 1-.149-.655.551.551 0 0 1 .722-.164l1.877 1.059V2.9a.5.5 0 1 1 1 0v4a.5.5 0 0 1-.017.13z" class="jsx-1364132950"></path></svg>Caundo</div>
+                                                <div class="h4"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" class="jsx-1364132950 eapp-events-calendar-time-icon me-1"><path d="M6 12A6 6 0 1 0 6 0a6 6 0 0 0 0 12zm.984-4.97a.454.454 0 0 1-.07.154l-.026.038a.5.5 0 0 1-.655.148L3.55 5.857a.454.454 0 0 1-.149-.655.551.551 0 0 1 .722-.164l1.877 1.059V2.9a.5.5 0 1 1 1 0v4a.5.5 0 0 1-.017.13z" class="jsx-1364132950"></path></svg>Cuando</div>
                                                 Sunday, December 01 15:00 - 17:00
                                             </div>
                                             <div class="col-6">
-                                                <div class="h4"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 10 12" class="jsx-654471426 eapp-events-calendar-location-icon"><path d="M5 12C1.667 9.174 0 6.84 0 5a5 5 0 1 1 10 0c0 1.84-1.667 4.174-5 7zm0-6a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" class="jsx-654471426"></path></svg>Donde</div>
+                                                <div class="h4"><svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 10 12" class="jsx-654471426 eapp-events-calendar-location-icon me-1"><path d="M5 12C1.667 9.174 0 6.84 0 5a5 5 0 1 1 10 0c0 1.84-1.667 4.174-5 7zm0-6a1 1 0 1 0 0-2 1 1 0 0 0 0 2z" class="jsx-654471426"></path></svg>Donde</div>
                                                 AT&T Park – San Francisco Giants24 Willie Mays Plaza, San Francisco, CA 94107, USA
+                                            </div>
+                                        </div>
+                                        <hr class="mt-2 mb-2">
+                                        <div class="row">
+                                            <div class="h4">Inversión</div>
+                                            <div class="">
+                                                <i class="fas fa-dollar-sign" style="color: #000000;"></i> 1000
                                             </div>
                                         </div>
                                     </div>
