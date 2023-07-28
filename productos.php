56a6693dc11081ec733b9abb6622d71874253265
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-07-28 11:07:00 -0600

    Crea archivo base

---
+++ productos.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php
+            require "main/head.php";
+        ?>
+    <title>Productos</title>
+</head>
+<body>
+    <div class="container-fluid contenedor-body">
+        <?php
+            require "main/header.php";
+        ?>
+    </div>
+</body>
+<?php
+    require "main/scripts.php";
+?>
+<footer class="footer mt-3">
+    <?php
+        require "main/footer.php";
+    ?>
+</footer>
+</html>
