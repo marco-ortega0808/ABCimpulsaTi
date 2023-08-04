fb0bb12469ee8d7c9424246f381b36b54a8a03b7
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-08-04 09:45:53 -0600

    Crea formato base

---
+++ softwareContpaqi.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <?php
+            require "main/head.php";
+        ?>
+    <title>Document</title>
+</head>
+<body>
+    <div class="container-fluid contenedor-body">
+        <?php
+            require "main/header.php";
+        ?>
+        <section>
+            <div class="container-fluid">
+    
+                <?php
+                    require "main/redes.php";
+                ?>
+            </div>
+        </section>
+    </div>
+</body>
+<?php
+    require "main/scripts.php";
+?>
+<footer class="footer mt-3">
+    <?php
+        require "main/footer.php";
+    ?>
+</footer>
+</html>
