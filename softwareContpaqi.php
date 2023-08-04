3d718ef846b5116cf1e630197f4cca682d48a94d
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-08-04 13:17:02 -0600

    Crea tarjeta de veneficios

--- softwareContpaqi.php
+++ softwareContpaqi.php
@@ -14,16 +14,24 @@
             require "main/header.php";
         ?>
         <section>
-            <div class="container-fluid">
-                <div class="row relleno-img">
-                    <img src="/img/contabilidad_folleto.png" alt="Contabilidad - Contpaqi" class="img-fluid">
+            <div class="row relleno-img">
+                <img src="/img/contabilidad_folleto.png" alt="Contabilidad - Contpaqi" class="img-fluid">
+            </div>
+            <h2 class="text-center mt-3 mb-3">Transformando números en éxitos financieros, simplificando tu gestión contable  </h2 class="text-center mt-3 mb-3">
+            <div class="row ">
+                <div class="col-6">
+                    <img src="/img/por.png" alt="" class="img-fluid">
                 </div>
-                <h2 class="text-center mt-3 mb-3">Transformando números en éxitos financieros, simplificando tu gestión contable  </h2 class="text-center mt-3 mb-3">
-                <div class="row">
-                    <?php
-                        require "main/redes.php";
-                    ?>
+                <div class="col-6 bg-azul">
+                    <h3 class="text-light text-center mt-3 mb-3">Venificios de Contabilidad -Contpaqi</h3>
+                  
                 </div>
+            </div>
+            
+            <div class="row">
+                <?php
+                    require "main/redes.php";
+                ?>
             </div>
         </section>
     </div>
