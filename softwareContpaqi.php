de2cd3371ebeae82b828e1ad9780e488654ce2fa
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-08-04 12:23:59 -0600

    Agrega titulo y da estilos globales

--- softwareContpaqi.php
+++ softwareContpaqi.php
@@ -18,10 +18,12 @@
                 <div class="row relleno-img">
                     <img src="/img/contabilidad_folleto.png" alt="Contabilidad - Contpaqi" class="img-fluid">
                 </div>
-    
-                <?php
-                    require "main/redes.php";
-                ?>
+                <h2 class="text-center mt-3 mb-3">Transformando números en éxitos financieros, simplificando tu gestión contable  </h2 class="text-center mt-3 mb-3">
+                <div class="row">
+                    <?php
+                        require "main/redes.php";
+                    ?>
+                </div>
             </div>
         </section>
     </div>
