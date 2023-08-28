90b11f8c27012169613d3fdf3ddf9db768a47dd1
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-08-28 12:38:55 -0600

    Da formato al texto

--- nosotros.php
+++ nosotros.php
@@ -13,25 +13,33 @@
         <?php
             require "main/header.php";
         ?>
-        <h1>
-            Nosotros
-        </h1>
-        <p class="text-secondary">
-            Somos una empresa distribuidora de software Contpaqi y soluciones tecnológicas para empresas. 
-        </p>
-        <p class="text-secondary">
-            Brindamos soluciones innovadoras y eficientes a nuestros clientes en el ámbito de la gestión empresarial. Ya que estamos comprometidos con el objetivo de ayudar a las organizaciones a optimizar sus operaciones, simplificar sus procesos y alcanzar el éxito en un mundo cada vez más digitalizado.
-        </p>
-        <p class="text-secondary">
-            Nuestro enfoque principal se centra en la distribución de software Contpaqi, una reconocida y poderosa suite de herramientas de gestión empresarial, lo que nos permite ofrecer a nuestros clientes una amplia gama de soluciones adaptadas a sus necesidades específicas. Ya sea que estén buscando herramientas de contabilidad, administración de inventarios, facturación electrónica o cualquier otro aspecto vital para su empresa, estamos aquí para ayudarles.
-        </p>
-        <p class="text-secondary">
-            En ABC Impulsa Ti, entendemos que cada negocio es único y tiene requisitos individuales. Por eso, trabajamos en estrecha colaboración con nuestros clientes para comprender sus desafíos y objetivos comerciales. Nuestro equipo de profesionales altamente capacitados y con amplia experiencia en el campo de los sistemas Conp está dedicado a brindar un servicio personalizado y asesoramiento experto para garantizar que nuestros clientes obtengan el máximo beneficio de las soluciones que ofrecemos.
-        </p>
+        <div class="row">
+            <h1>
+                Nosotros
+            </h1>
+            <p class="text-secondary text-justify">
+                Somos una empresa distribuidora de <strong>software Contpaqi y soluciones tecnológicas</strong> para empresas. 
+            </p>
+            <div class="col-6">
+                <p class="text-secondary text-justify">
+                    Brindamos soluciones innovadoras y eficientes a nuestros clientes en el ámbito de la gestión empresarial. Ya que estamos comprometidos con el objetivo de ayudar a las organizaciones a optimizar sus operaciones, simplificar sus procesos y alcanzar el éxito en un mundo cada vez más digitalizado.
+                </p>
+                <p class="text-secondary text-justify">
+                    Nuestro enfoque principal se centra en la distribución de software Contpaqi, una reconocida y poderosa suite de herramientas de gestión empresarial, lo que nos permite ofrecer a nuestros clientes una amplia gama de soluciones adaptadas a sus necesidades específicas. Ya sea que estén buscando herramientas de contabilidad, administración de inventarios, facturación electrónica o cualquier otro aspecto vital para su empresa, estamos aquí para ayudarles.
+                </p>
+            </div>
+            <div class="col-6">
+                <img src="/img/11641780_4783003.svg" alt="">
+            </div>
+            <p class="text-secondary text-justify">
+                En <Strong>ABC Impulsa Ti</Strong>, entendemos que cada negocio es único y tiene requisitos individuales. Por eso, trabajamos en estrecha colaboración con nuestros clientes para comprender sus desafíos y objetivos comerciales. Nuestro equipo de profesionales altamente capacitados y con amplia experiencia en el campo de los sistemas Conp está dedicado a brindar un servicio personalizado y asesoramiento experto para garantizar que nuestros clientes obtengan el máximo beneficio de las soluciones que ofrecemos.
+            </p>
+            <?php
+                require "main/redes.php";
+            ?>
+        </div>
     </div>
-    <?php
-        require "main/redes.php";
-    ?>
+    
 </body>
     <?php
         require "main/scripts.php";
