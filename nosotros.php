3dea20b816dcde5c37de657bcb063af28a17ed20
Marco Antonio Ortega Trejo <marcoantoniot089@gmail.com>
2023-08-29 13:19:39 -0600

    Responsivo dispositivos SM

--- nosotros.php
+++ nosotros.php
@@ -14,26 +14,26 @@
             require "main/header.php";
         ?>
         <div class="row">
-            <h1>
-                Nosotros
-            </h1>
-            <p class="text-secondary text-justify">
-                Somos una empresa distribuidora de <strong>software Contpaqi y soluciones tecnológicas</strong> para empresas. 
-            </p>
-            <div class="col-6">
+            <div class="col-lg-6 col-sm-12">
+                <h1>
+                    Nosotros
+                </h1>
+                <p class="text-secondary text-justify">
+                    Somos una empresa distribuidora de <strong>software Contpaqi y soluciones tecnológicas</strong> para empresas. 
+                </p>
                 <p class="text-secondary text-justify">
                     Brindamos soluciones innovadoras y eficientes a nuestros clientes en el ámbito de la gestión empresarial. Ya que estamos comprometidos con el objetivo de ayudar a las organizaciones a optimizar sus operaciones, simplificar sus procesos y alcanzar el éxito en un mundo cada vez más digitalizado.
                 </p>
                 <p class="text-secondary text-justify">
                     Nuestro enfoque principal se centra en la distribución de software Contpaqi, una reconocida y poderosa suite de herramientas de gestión empresarial, lo que nos permite ofrecer a nuestros clientes una amplia gama de soluciones adaptadas a sus necesidades específicas. Ya sea que estén buscando herramientas de contabilidad, administración de inventarios, facturación electrónica o cualquier otro aspecto vital para su empresa, estamos aquí para ayudarles.
                 </p>
+                <p class="text-secondary text-justify">
+                    En <Strong>ABC Impulsa Ti</Strong>, entendemos que cada negocio es único y tiene requisitos individuales. Por eso, trabajamos en estrecha colaboración con nuestros clientes para comprender sus desafíos y objetivos comerciales. Nuestro equipo de profesionales altamente capacitados y con amplia experiencia en el campo de los sistemas Conp está dedicado a brindar un servicio personalizado y asesoramiento experto para garantizar que nuestros clientes obtengan el máximo beneficio de las soluciones que ofrecemos.
+                </p>
             </div>
-            <div class="col-6">
-                <img src="/img/11641780_4783003.svg" alt="">
+            <div class="col-lg-6 col-sm-12">
+                <img src="/img/11641780_4783003.svg" class="img-fluid" alt="">
             </div>
-            <p class="text-secondary text-justify">
-                En <Strong>ABC Impulsa Ti</Strong>, entendemos que cada negocio es único y tiene requisitos individuales. Por eso, trabajamos en estrecha colaboración con nuestros clientes para comprender sus desafíos y objetivos comerciales. Nuestro equipo de profesionales altamente capacitados y con amplia experiencia en el campo de los sistemas Conp está dedicado a brindar un servicio personalizado y asesoramiento experto para garantizar que nuestros clientes obtengan el máximo beneficio de las soluciones que ofrecemos.
-            </p>
             <?php
                 require "main/redes.php";
             ?>
